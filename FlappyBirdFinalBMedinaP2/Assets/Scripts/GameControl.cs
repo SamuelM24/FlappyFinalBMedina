c26187109d9dd3b6fd8acd5ed527c135ff50059b
SamuelM24 <49ersownrams@gmail.com>
2022-12-17 10:37:29 -0800

    ok

--- FlappyBirdFinalBMedinaP2/Assets/Scripts/GameControl.cs
+++ FlappyBirdFinalBMedinaP2/Assets/Scripts/GameControl.cs
@@ -6,7 +6,7 @@ using UnityEngine;
     
     public static GameControl instance;
     public GameObject gameOverText;
-    public boool gameOver = false;
+    public bool gameOver = false;
     
     // Start is called before the first frame update
     void Awake()
